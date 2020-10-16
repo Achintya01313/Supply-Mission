021ef377623ebdfca7977091168c799dad866ac5
Achintya01313 <72142052+Achintya01313@users.noreply.github.com>
2020-10-16 15:30:46 +0530

    Add files via upload

---
+++ keyPressed.js
@@ -0,0 +1,8 @@
+function keyPressed() {
+	if (keyPressed(DOWN_ARROW)) {
+		packageSprite.velocityY = 3;
+
+
+		// Look at the hints in the document and understand how to make the package body fall only on
+	}
+}
